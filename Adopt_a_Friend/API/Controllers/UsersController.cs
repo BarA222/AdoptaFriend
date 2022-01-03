cc9fd473f56ad4dcfa0fcc3eecc081dadeb812af
BarAz <barazriel2@gmail.com>
2022-01-03 11:48:26 +0200

    End of section 4

--- Adopt_a_Friend/API/Controllers/UsersController.cs
+++ Adopt_a_Friend/API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,12 +21,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
         {
             var users = await _context.Users.ToListAsync();
             return users;
         }
 
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUserAsync(int id)
         {
